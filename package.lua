993ea56ae07cd865626af5f3215822f2a11bc435
tionis <git@tionis.dev>
2023-12-03 01:17:09 +0100

    initial commit

---
+++ package.lua
@@ -0,0 +1,14 @@
+return {
+  name = "tionis/geth",
+  version = "0.0.1",
+  description = "Small scriptable discord bot",
+  tags = { "discord" },
+  license = "MIT",
+  author = { name = "tionis", email = "geth@tionis.dev" },
+  homepage = "https://github.com/tionis/geth",
+  dependencies = { "SinisterRectus/discordia" },
+  files = {
+    "**.lua",
+    "!test*"
+  }
+}
